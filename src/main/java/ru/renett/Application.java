7e12cce184cd9cd41472d5cca3a46f849fab9771
renett_t <rtapkina@gmail.com>
2022-05-07 00:06:06 +0300

    configuration

--- src/main/java/ru/renett/Application.java
+++ src/main/java/ru/renett/Application.java
@@ -2,9 +2,17 @@ package ru.renett;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(
                 Application.class, args
