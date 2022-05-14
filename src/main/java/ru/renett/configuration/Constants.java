cbabe9a07589e33de107a5f14b74348637d381bc
renett_t <rtapkina@gmail.com>
2022-05-14 14:33:31 +0300

    i18n - some messages, config

--- src/main/java/ru/renett/configuration/Constants.java
+++ src/main/java/ru/renett/configuration/Constants.java
@@ -3,30 +3,17 @@ package ru.renett.configuration;
 import java.io.File;
 
 public class Constants {
-    public static final String HASHING_ALGORITHM_NAME = "SHA-256";
-
-    public static final String DB_URL = "db.url";
-    public static final String DB_USERNAME = "db.username";
-    public static final String DB_PASSWORD = "db.password";
-    public static final String DB_DRIVER = "db.driver";
-    public static final String DB_POOL_SIZE = "db.max-pool-size";
-
-    public static final String CNTX_SECURITY_SERVICE = "securityService";
-    public static final String CNTX_ARTICLE_GET_SERVICE = "articleGetService";
-    public static final String CNTX_ARTICLE_SAVE_SERVICE = "articleSaveService";
-    public static final String CNTX_VK_SERVICE = "vkSerice";
-    public static final String CNTX_USER_SERVICE = "userService";
-    public static final String CNTX_PREFERENCES_MANAGER = "preferencesManager";
-    public static final String CNTX_REQUEST_VALIDATOR = "requestValidator";
-
-    public static final String COOKIE_AUTHORIZED_NAME = "uuid-user";
+    // COOKIE
     public static final String COOKIE_LAST_VIEWED_ARTICLE = "lwai";
-    public static final String SESSION_USER_ATTRIBUTE_NAME = "auth_user";
-    public static final String REQUEST_ATTRIBUTE_AUTHORIZED = "authorized";                          // renaming here is dangerous!! then edit jsp files
+    public static final String COOKIE_LOCALE = "locale";
+    public static final int COOKIE_LOCALE_MAX_AGE = 60 * 60 * 24 * 365;
+    public static final String LOCALE_CHANGE_PARAM = "lang";
 
-    public static final String STORAGE_URL = "resources" + File.separator + "articles";
+    // SOURCES
+    public static final String MESSAGES_SOURCE = "classpath:"  + File.separator + "messages";
+
+    public static final String STORAGE_URL = File.separator + "static" + File.separator + "thumbnails";
     public static final String CHAR_ENCODING = "UTF-8";
-    public static final String CHAR_ENCODING_ATTR_NAME = "encoding";
     public static final String DEFAULT_THUMBNAIL = "guitar-background.jpg";
 
     // SECURITY CONFIG
@@ -34,6 +21,7 @@ public class Constants {
 
     // ATTRIBUTES FOR VIEW
     public static final String IS_AUTHENTICATED_ATTR = "isAuthenticated";
+    public static final String SIGN_UP_FORM_ATTR = "signUpForm";
     public static final String USER_ATTR = "user";
     public static final String SEARCH_TAG_ATTR = "searchTag";
     public static final String LIKED_ARTICLES_ATTR = "likedArticles";
--- src/main/java/ru/renett/configuration/WebMvcConfiguration.java
+++ src/main/java/ru/renett/configuration/WebMvcConfiguration.java
@@ -1,11 +1,20 @@
 package ru.renett.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import ru.renett.interceptor.AuthenticationInterceptor;
+
+import java.util.Locale;
+
+import static ru.renett.configuration.Constants.*;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -15,5 +24,31 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor);
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setCookieName(COOKIE_LOCALE);
+        cookieLocaleResolver.setCookieMaxAge(COOKIE_LOCALE_MAX_AGE);
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName(LOCALE_CHANGE_PARAM);
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGES_SOURCE);
+        messageSource.setDefaultEncoding(CHAR_ENCODING);
+        //        messageSource.setConcurrentRefresh(true);
+        return messageSource;
     }
 }
